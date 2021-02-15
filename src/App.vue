110be0cff5feef5d4ebad49f51d00b1bd421d27b
wanavy <nacanori@gmail.com>
2021-02-16 03:01:33 +0900

    About modal into header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
-    <Header />
-    <Main />
+    <Header v-bind:showAbout="state.showAbout" @close="toggleAbout(false)" @open="toggleAbout(true)" />
+    <Main @close="toggleAbout(false)" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
+
+interface State {
+  showAbout: boolean;
+}
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
     Main,
+  },
+  setup() {
+    const state = reactive<State>({
+      showAbout: false,
+    })
+    const toggleAbout = (show: boolean|undefined) => {
+      if (typeof show === undefined) {
+        state.showAbout = !state.showAbout
+      } else {
+        state.showAbout = !!show
+      }
+    }
+    return {
+      state,
+      toggleAbout,
+    }
   }
 });
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="Header">
+  <div class="Header" @click="$emit('close')" v-observe-visibility="visibilityChanged">
     <div class="Header-item">
       <span class="logo mr-2">{{ logo }}</span>
       <span class="title">MATERIAL ICONS SEARCH</span>
     </div>
     <div class="Header-item Header-item--full">
     </div>
+    <div class="Header-link position-relative mr-3" @click.stop="$emit(showAbout ? 'close' : 'open')">
+      About
+      <div class="Popover right-0 mt-2 position-absolute text-gray-dark text-normal ws-normal"
+        :class="{'v-hidden': !showAbout}">
+        <div class="Popover-message Popover-message--top-right text-left p-3 Box box-shadow-large wb-break-all">
+          <p class="f4">A site to help web developers find <a href="https://material.io/resources/icons/" target="_blank">Google Material Icons</a> quickly and easily.</p>
+          <a class="f5" href="https://github.com/underground/material-icons/issues/new" target="_blank">Look forward to hearing your ideas. </a>
+        </div>
+      </div>
+    </div>
     <a href="https://material.io/develop/web/getting-started" target="_blank" class="Header-link mr-3">
-      GET STARTED
+      Get Started
     </a>
     <a href="https://github.com/underground/material-icons" target="_blank" class="Header-link mr-0">
+      Github
       <svg class="octicon octicon-mark-github mr-2" viewBox="0 0 16 16" version="1.1" height="16" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
     </a>
   </div>
@@ -20,10 +31,22 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Header',
-  setup() {
+  props: {
+    showAbout: {
+      type: Boolean,
+      required: true
+    },
+  },
+  setup(props, { emit }) {
     const logo = "</>"
+    const visibilityChanged = (isVisible: boolean) => {
+      if (!isVisible) {
+        emit('close')
+      }
+    }
     return {
-      logo
+      logo,
+      visibilityChanged,
     }
   },
 });
@@ -31,6 +54,7 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 @import "@primer/css/header/index.scss";
+@import "@primer/css/popover/index.scss";
 @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@500&display=swap');
 
 .logo {
@@ -44,6 +68,9 @@ export default defineComponent({
   font-size: 1.1em;
   letter-spacing: .3rem;
   color: white;
+}
+.Header-link {
+  cursor: pointer;
 }
 
 </style>
