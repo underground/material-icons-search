0c202260873c64ac6a3c9af5bed6858f25a49240
NXN <nacanori@gmail.com>
2021-02-17 23:34:12 +0900

    Change fonts

--- src/App.vue
+++ src/App.vue
@@ -43,10 +43,12 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap');
 @import '@primer/css/core/index.scss';
 
 html, body {
   height: 100%;
+  font-family: 'Open Sans', sans-serif;
 }
 body {
   margin: 0;
