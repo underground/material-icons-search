e39b3011cb0d2f2e71f9f0852db452eddb77f6f3
wanavy <nacanori@gmail.com>
2021-02-19 22:43:24 +0900

    Add gtag

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { createApp } from 'vue'
 import { ObserveVisibility } from 'vue-observe-visibility'
+import VueGtag from 'vue-gtag-next'
 import App from './App.vue'
 
-createApp(App)
-  // https://github.com/Akryum/vue-observe-visibility/issues/219#issuecomment-688586495
-  .directive("observe-visibility", {
-    beforeMount: (el, binding, vnode) => {
-      (vnode as any).context = binding.instance
-      ObserveVisibility.bind(el, binding, vnode)
-    },
-    updated: ObserveVisibility.update,
-    unmounted: ObserveVisibility.unbind,
+const app = createApp(App)
+// https://github.com/Akryum/vue-observe-visibility/issues/219#issuecomment-688586495
+app.directive("observe-visibility", {
+  beforeMount: (el, binding, vnode) => {
+    (vnode as any).context = binding.instance
+    ObserveVisibility.bind(el, binding, vnode)
+  },
+  updated: ObserveVisibility.update,
+  unmounted: ObserveVisibility.unbind,
+})
+if (process.env.VUE_APP_GA_TRACKING_ID) {
+  app.use(VueGtag, {
+    property: {
+      id: process.env.VUE_APP_GA_TRACKING_ID
+    }
   })
-  .mount('#app')
+}
+app.mount('#app')
