69763fd1a129eef6f935f8058eec9b1bbcf3312d
NXN <nacanori@gmail.com>
2022-05-21 14:47:31 +0900

    Move constraint to data file

--- src/types/icon.d.ts
+++
@@ -1,8 +0,0 @@
-export default interface Icon {
-  font: string;
-  name: string;
-  codepoint: string;
-  category: string | undefined;
-  tags: string[];
-  popularity: number;
-}
---
+++ src/types/index.d.ts
@@ -0,0 +1,17 @@
+export interface Font = {
+  font: string,
+  label: string,
+  url: string,
+}
+
+export interface Icon {
+  font: string;
+  name: string;
+  codepoint: string;
+  category: string | undefined;
+  tags: string[];
+  popularity: number;
+}
+
+export const FONTS = ["filled", "outlined", "rounded", "twoTone", "sharp"]
+export interface FontType = typeof FONTS[number]
