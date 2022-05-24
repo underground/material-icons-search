d463e67e64fa249f8b90e0a7e31251218d02b68a
NXN <nacanori@gmail.com>
2022-05-24 10:47:05 +0900

    Set custom element use to config
    - [Vue warn]: Failed to resolve component: details-dialog. If this is a native custom element, make sure to exclude it from component resolution via compilerOptions.isCustomElement.

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,17 @@ module.exports = {
         additionalData: '@import "./src/styles/global.scss";'
       }
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => ({
+        ...options,
+        compilerOptions: {
+          // treat any tag that starts with ion- as custom elements
+          isCustomElement: tag => tag.startsWith("details-dialog"),
+        }
+      }))
   }
 };
